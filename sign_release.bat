cc0393570b1fcf7bb2cab4d73652db97cb6e3ded
NicoAvanzDev <35104310+NicoAvanzDev@users.noreply.github.com>
2018-03-26 21:38:12 +0200

    General edit

--- sign_release.bat
+++ sign_release.bat
@@ -1,7 +1,11 @@
+ECHO off
+cls
 cd .\platforms\android\app\build\outputs\apk
-ECHO Signing apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore nc91parma.keystore ./release/app-release-unsigned.apk nc91parma
 cd release
-zipalign -v 4 app-release-unsigned.apk Nc91Parma.apk
+if EXIST "nc91parma.apk" (
+	del nc91parma.apk
+)
+zipalign -v 4 app-release-unsigned.apk nc91parma.apk
 pause
 exit
